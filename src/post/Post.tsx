d2e5181b7a212fc9cf6c3f063bad7bbc7f4ed77a
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-06-10 20:50:02 +0300

    new branch test commit

--- src/post/Post.tsx
+++ src/post/Post.tsx
@@ -6,6 +6,8 @@ export const Post = () => {
             <h2>test 11</h2>
             <h2>test 11</h2>
             <h2>test 11</h2>
+            <h2>test 11</h2>
+            <h2>test 11</h2>
         </div>
     )
 }
